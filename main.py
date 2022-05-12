ddf5145a5c697da6c04be28be2b059b6b8d45e89
Evan Dartez <98479231+esdartez@users.noreply.github.com>
2022-05-11 21:59:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,9 @@ def make_huffman_tree(f):
     # create a new node z with x and y as children,
     # insert z into the priority queue (using an empty character "")
     while (p.qsize() > 1):
-        # TODO
+        l = p.get()
+        r = p.get()
+        p.put(TreeNode(l, r, (l.data[0]+r.data[0], "")))
         
     # return root of the tree
     return p.get()
@@ -42,20 +44,32 @@ def make_huffman_tree(f):
 # perform a traversal on the prefix code tree to collect all encodings
 def get_code(node, prefix="", code={}):
     # TODO - perform a tree traversal and collect encodings for leaves in code
-    pass
+    if ((node.left == None) and (node.right == None)):
+        code[node.data[1]] = prefix
+    if (node.left != None):
+        get_code(node.left,prefix+"0", code)
+    if (node.right != None):
+        get_code(node.right,prefix+"1", code)
+    return(code)
 
 # given an alphabet and frequencies, compute the cost of a fixed length encoding
 def fixed_length_cost(f):
-    # TODO
-    pass
-
+    num_bits = math.ceil(math.log2(len(f.keys())))
+    return(sum([num_bits*f[x] for x in f.keys()]))
 # given a Huffman encoding and character frequencies, compute cost of a Huffman encoding
 def huffman_cost(C, f):
     # TODO
-    pass
+    return(sum([len(C[x])*f[x] for x in f.keys()]))
 
-f = get_frequencies('f1.txt')
-print("Fixed-length cost:  %d" % fixed_length_cost(f))
-T = make_huffman_tree(f)
-C = get_code(T)
-print("Huffman cost:  %d" % huffman_cost(C, f))
+for fname in ['alice29.txt', 'asyoulik.txt', 'f1.txt', 'fields.c', 'grammar.lsp']:
+    print(fname)
+    f = get_frequencies(fname)
+    fc =fixed_length_cost(f)
+    print("Fixed-length cost:  %d" % fc)
+
+    T = make_huffman_tree(f)
+    C = get_code(T)
+    hc = huffman_cost(C, f)
+    print("Huffman cost:  %d" % hc)
+    h2f = hc/fc
+    print("Huffman v.s. Fixed-length cost:  %f" % h2f)
